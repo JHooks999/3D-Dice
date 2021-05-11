58e8c3bba0eacb4839a0d2a3a3efe26a6d7985da
Justin Hooks <justinhj999@gmail.com>
2021-05-11 12:10:23 -0700

    created diceValu function and updateScore function

--- app.js
+++ app.js
@@ -2,3 +2,25 @@ let playerScores = [0,0];
 let roundScore = 0;
 let currentPlayer = 0;
 let diceRoll = 0;
+
+function diceValue() {
+  //This function will generate the dice value of the currentPlayer
+   // One dice goes up to 6
+  diceRoll = Math.floor( Math.random() * 6 );
+  //console.log("You rolled" + diceRoll);
+  console.log("You rolled", diceRoll)
+  }
+  //diceValue()
+  //console.log("You rolled", diceRoll)
+  
+  function updateScore() {
+    //This function will change the score of the currentplayer
+    roundScore += diceRoll
+    
+    
+    
+    
+    
+    //roll dice and add diceRoll to roundScore
+    //else if player chose hold switchPlayers
+  }
